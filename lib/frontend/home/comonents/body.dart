e51b7d66b65d41591d9e55022ab41388442f8abc
Akshay Vhatkar <akshay0706vhatkar@gmail.com>
2023-04-21 21:39:13 +0530

    Changed API Key

--- lib/frontend/home/comonents/body.dart
+++ lib/frontend/home/comonents/body.dart
@@ -1,10 +1,33 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeBody extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeBody extends StatefulWidget {
   const HomeBody({super.key});
 
   @override
+  State<HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition mumbai = CameraPosition(
+    target: LatLng(19.0760, 72.8777),
+    zoom: 13,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: mumbai,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
   }
 }
--- lib/frontend/home/home.dart
+++ lib/frontend/home/home.dart
@@ -1,32 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:routing/frontend/home/comonents/body.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final Completer<GoogleMapController> _controller = Completer();
-
-  static const CameraPosition mumbai = CameraPosition(
-    target: LatLng(19.0760, 72.8777),
-    zoom: 13,
-  );
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: GoogleMap(
-      mapType: MapType.hybrid,
-      initialCameraPosition: mumbai,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-    ));
+    return const Scaffold(
+      body: HomeBody(),
+    );
   }
 }
